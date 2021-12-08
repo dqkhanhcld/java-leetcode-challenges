8aa7eed0ad750f52fd375858467ec7033d89ca44
dqkhanhcld <95413365+dqkhanhcld@users.noreply.github.com>
2021-12-09 02:09:19 +0700

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -14,6 +14,26 @@ class Solution {
         }
         
         
-          throw new IllegalArgumentException("match");
+          throw new IllegalArgumentException("not match");
+    }
+}
+
+// hash table solution: (cách giải quyết với hash table)
+
+
+ class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map <Integer, Integer> num_map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            
+            if(num_map.containsKey(complement)){
+                return new int[] {num_map.get(complement), i};
+            }
+            num_map.put(nums[i], i);
+            
+        }
+        
+        throw new IllegalArgumentException("no match found!!");
     }
 }
